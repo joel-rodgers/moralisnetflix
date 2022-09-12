a5c8566df2663fbdb04aed1b8390d37005a372d4
joel-rodgers <joel.kobara@strathmore.edu>
2022-09-12 18:30:59 +0300

    Home Page Complete

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import "./Home.css";
 import { Logo } from '../images/Netflix';
-import {ConnectButton, Icon, TabList, Tab} from "web3uikit";
+import {ConnectButton, Icon, TabList, Tab, Button} from "web3uikit";
+import { movies } from "../helpers/library";
 
 const Home = () => {
 
@@ -17,10 +18,48 @@ return(
   </div>
   <div className="topBanner">
      <TabList defaultActiveKey={1} tabStyle="bar">
-        <Tab tabKey={1} tabName={"Movies"}></Tab>
+        <Tab tabKey={1} tabName={"Movies"}>
+          <div className="scene">
+            <img src={movies[0].Scene} className="sceneImg"></img>
+            <img className="sceneLogo" src={movies[0].Logo}></img>
+            <p className="sceneDesc">{movies[0].Description}</p>
+            <div className="playButton">
+            <Button
+                icon="chevronRightX2"
+                text="Play"
+                theme="secondary"
+                type="button"
+              />
+            <Button
+                icon="plus"
+                text="Add to My List"
+                theme="translucent"
+                type="button"
+              />
+            </div>
+          </div>
+
+          <div className="title">
+            Movies
+          </div>
+
+          <div className="thumbs">
+            {movies &&
+            movies.map((e)=> {
+              return(
+                <img
+                src={e.Thumnbnail}
+                className="thumbnail"
+                >  
+                </img>
+              )
+            })
+            }
+          </div>
+
+        </Tab>
         <Tab tabKey={2} tabName={"Series"} isDisabled={true}></Tab>
         <Tab tabKey={3} tabName={"MyList"}></Tab>
-        <Tab tabKey={3} tabName={"Documentaries"}></Tab>
      </TabList>
   </div>
   </>
